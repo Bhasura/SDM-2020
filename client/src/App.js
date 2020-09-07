c99683849641b6c037641c0c15fba6fd1aeff147
sherin-jacob <41213885+sherin-jacob@users.noreply.github.com>
2020-09-07 18:58:32 +1200

    registration front-end

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,11 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import Register from './components/Register.jsx';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          <code>Hello</code>
-          <h3>SHERIN WAS HERE 05/09</h3>
-          <h3>NISHANT WAS HERE</h3>
-          <h3>JESPHILMAR WAS HERE</h3>
-          <h3>BRIAN WAS HERE</h3>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Register />
   );
 }
 
---
+++ client/src/pages/Signin.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Register from '/client/src/components/Register.jsx';
+
+export class Register extends Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    render () {
+        return () => {
+            
+        }
+    }
+
+}
