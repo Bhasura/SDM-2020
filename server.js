9b6a71ecf1b99a5f3c6eb28a7fe80accc869a769
Nishant Patel <nishantpatel1842@gmail.com>
2020-09-09 17:36:44 +1200

    Checks if environment is production or staging in server.js

--- server.js
+++ server.js
@@ -27,6 +27,16 @@ var Users = require('./routes/Users')
 
 app.use('/users', Users)
 
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'client/build')));
+
+// Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
+
 app.listen(port, function() {
   console.log('Server is running on port: ' + port)
 })
