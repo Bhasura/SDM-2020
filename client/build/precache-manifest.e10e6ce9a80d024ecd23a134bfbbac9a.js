6be9b930000ad7f4e2d65408562609c46d2cde35
Bhasura <brian.desilva5@gmail.com>
2020-09-28 18:20:58 +1300

    new build

---
+++ client/build/precache-manifest.e10e6ce9a80d024ecd23a134bfbbac9a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9754b4f0be1fa01071f4eeaedde96361",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cfd2bffef9b563e3d3d2",
+    "url": "/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "60bdb7e7028ebd96fb0b",
+    "url": "/static/js/2.7b29d221.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.7b29d221.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cfd2bffef9b563e3d3d2",
+    "url": "/static/js/main.b705f0b2.chunk.js"
+  },
+  {
+    "revision": "5bfab23557c5d3618c78",
+    "url": "/static/js/runtime-main.3ae12bfd.js"
+  }
+]);
