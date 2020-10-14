9a9888dfa2af193c8565457ca1731cc504c88923
Bhasura <brian.desilva5@gmail.com>
2020-10-14 22:33:45 +1300

    #152 changed date slider to noUISlider which doesnt allow overlapping

--- client/src/components/DateSlider.jsx
+++ client/src/components/DateSlider.jsx
@@ -1,37 +1,47 @@
-import React, {Component} from "react";
-import { Typography, Slider, Paper, Grid, withStyles, Container } from "@material-ui/core";
+import React, { Component } from "react";
+import {
+  Typography,
+  Slider,
+  Paper,
+  Grid,
+  withStyles,
+  Container,
+} from "@material-ui/core";
 import DateRadioButtons from "./DateRadioButtons";
+import Nouislider from "nouislider-react";
+import "nouislider/distribute/nouislider.css";
+import wNumb from "wnumb";
 
 const Title = withStyles({
-  root : {
+  root: {
     color: "#52B788",
-    marginTop: 5
+    marginTop: 5,
   },
-})(Typography)
+})(Typography);
 
 const CustomPaper = withStyles({
-  root : {
+  root: {
     backgroundColor: "#D8F3DC",
     //width: 800
     width: 700,
-    marginBottom: 10
+    marginBottom: 10,
   },
-})(Paper)
+})(Paper);
 
 const CustomSlider = withStyles({
-  root : {
+  root: {
     color: "#52B788",
     width: 200,
-    marginTop: 28
-  }
-})(Slider)
+    marginTop: 28,
+  },
+})(Slider);
 
 const RadioGrid = withStyles({
-  root : {
+  root: {
     // paddingLeft: 50,
     // paddingTop: 10
   },
-})(Grid)
+})(Grid);
 
 // const ResetButton = withStyles({
 //   root: {
@@ -41,71 +51,77 @@ const RadioGrid = withStyles({
 // })(Button);
 
 const DateWrapper = withStyles({
-  root : {
-    maxWidth: 700, 
+  root: {
+    maxWidth: 700,
     padding: 0,
-    marginLeft: 10
-  }
-})(Container)
+    marginLeft: 10,
+  },
+})(Container);
 
 const RadioButtonWrapper = withStyles({
-  root : {
+  root: {
     marginLeft: 30,
     marginTop: 30,
-    color: "#52B788"
-  }
-})(Container)
+    color: "#52B788",
+  },
+})(Container);
 
 export default class RangeSlider extends Component {
   constructor() {
     super();
     this.state = {
-      value : [2015,2020]
+      value: [2015, 2020],
     };
   }
 
-  valuetext = (value) =>{
+  valuetext = (value) => {
     return `${value}`;
-  }
+  };
 
   handleChange = (event, newValue) => {
-    this.setValue(newValue);
-    
+    this.setValue(event);
+    console.log(event);
   };
 
   setValue = (newValue) => {
-    this.setState({value: newValue}, () => {
+    this.setState({ value: newValue }, () => {
       this.props.callbackFromParent(this.state.value);
     });
-  }
+  };
 
   dateSliderCallback = (dataFromRadioButtons) => {
     let newValue = [];
-    if(dataFromRadioButtons === "0") {
+    if (dataFromRadioButtons === "0") {
       newValue = [2020, 2020];
-    } else if(dataFromRadioButtons === "5") {
+    } else if (dataFromRadioButtons === "5") {
       newValue = [2015, 2020];
     } else {
       newValue = [1990, 2020];
-    }; 
-    this.setValue(newValue)
+    }
+    this.setValue(newValue);
     this.props.callbackFromParent(this.state.value);
-  }
+  };
 
   render() {
-
     return (
-      <CustomPaper elevation={7} variant="outlined" >
+      <CustomPaper elevation={7} variant="outlined">
         <Grid container direction="row">
           <Grid item>
             <DateWrapper>
-              <Title
-                id="range-slider"
-                gutterBottom
-              >
+              <Title id="range-slider" gutterBottom>
                 Select Date Range
               </Title>
-              <CustomSlider
+              <Nouislider
+                start={this.state.value}
+                onSlide={this.handleChange}
+                tooltips={true}
+                format={wNumb({ decimals: 0 })}
+                range={{
+                  min: 1990,
+                  max: 2020,
+                }}
+              />
+              {/* <CustomSlider
                 id="sliderTest"
                 value={this.state.value}
                 onChange={this.handleChange}
@@ -114,12 +130,12 @@ export default class RangeSlider extends Component {
                 getAriaValueText={this.valuetext}
                 min={1990}
                 max={2020}
-              />
+              /> */}
             </DateWrapper>
           </Grid>
-          <RadioGrid item >         
+          <RadioGrid item>
             <RadioButtonWrapper>
-              <DateRadioButtons radioButtonCallback={this.dateSliderCallback}/>
+              <DateRadioButtons radioButtonCallback={this.dateSliderCallback} />
             </RadioButtonWrapper>
           </RadioGrid>
           {/* <Grid item >
